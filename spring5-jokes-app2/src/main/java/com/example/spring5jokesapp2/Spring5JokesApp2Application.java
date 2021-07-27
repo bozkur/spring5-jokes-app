423e1401794ea85e685eb52ab4dcfb62c3b17ead
Cevher Cemal Bozkur <bozkur@gmail.com>
2021-07-27 15:06:33 +0300

    Spring web project is created with Spring Initializer

---
+++ spring5-jokes-app2/src/main/java/com/example/spring5jokesapp2/Spring5JokesApp2Application.java
@@ -0,0 +1,13 @@
+package com.example.spring5jokesapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokesApp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokesApp2Application.class, args);
+    }
+
+}
---
+++ spring5-jokes-app2/src/test/java/com/example/spring5jokesapp2/Spring5JokesApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5jokesapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesApp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
